4d64ce006a1edeab0645f3b7d8f2dee6d0210206
CarinaNovais <carinapereiraN@outlook.com>
2021-09-11 10:03:11 +0900

    Adicionando pluguins de vibração e qr code

--- www/components/script.js
+++ www/components/script.js
@@ -5,7 +5,10 @@ window.onload = function (){
     function retorno(){
 
     }
-    navigator.notification.alert("alarme de teste", retorno,"titulo do alerta", "SIM");
+    navigator.notification.alert("alarme de teste",
+    retorno,
+    "titulo do alerta", 
+    "SIM");
   });
 
    document.querySelector("#alerta2").addEventListener("click", function (){
@@ -17,14 +20,56 @@ window.onload = function (){
       }
 
     }
-    navigator.notification.confirm("Escolha A ou B", 
-    verifica,
-    "Opções de escolha",
-     ['A','B'], 
-     );
-  });
+      navigator.notification.confirm("Escolha A ou B", 
+        verifica,
+          "Opções de escolha",
+             ['A','B'], 
+      );
+    });
 
-  document.querySelector("#alerta2").addEventListener("click", function (){
-    navigator.notification.beep(2);
-  });
+    document.querySelector("#beep").addEventListener("click", function (){
+      navigator.notification.beep(2);
+    });
+
+    document.querySelector("#vibrar").addEventListener("click", function (){
+      navigator.vibrate(3000)
+    });
+
+    document.querySelector("#code").addEventListener("click", function (){
+
+      cordova.plugins.barcodeScanner.scan(
+      function (result) {
+        if (result.cancelled){
+          navigator.notification.alert("cancelado");
+        }else{
+           document.querySelector("#resultado").textContent = result.text;
+        }
+
+          /*alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);*/
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+
+
+      
+    });
+
 }
